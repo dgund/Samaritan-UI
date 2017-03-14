cbf8a5f3412d7c5759300bf09c9bcb46574e691d
Devin Gund <devin@dgund.com>
2017-03-14 11:29:40 -0400

    Added main SamaritanUI class. Currently displays empty window.

--- samaritan.py
+++ samaritan.py
@@ -10,3 +10,14 @@
 # The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
 
+import tkinter as tk
+
+class SamaritanUI(tk.Frame):
+    def __init__(self, parent, *args, **kwargs):
+        tk.Frame.__init__(self, parent, *args, **kwargs)
+        self.parent = parent
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    SamaritanUI(root).pack(side="top", fill="both", expand=True)
+    root.mainloop()
